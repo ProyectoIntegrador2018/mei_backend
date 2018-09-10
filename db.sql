a626282c819f363066aa8a8a9945917fceaeec51
Gerardo Gálvez <galvezgerardo01@gmail.com>
2018-09-10 15:10:56 -0500

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE MEI;
+
+USE MEI;
+
+CREATE TABLE Users (
+	userID INT AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (userID)
+);
+
+CREATE TABLE Project (
+	projectID INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    org VARCHAR(255) NOT NULL,
+    creationDate DATETIME NOT NULL,
+    context VARCHAR(512) NOT NULL,
+    owner INT NOT NULL,
+    FOREIGN KEY (owner) REFERENCES Users (userID),
+    PRIMARY KEY (projectID)
+);
+
+CREATE TABLE Session (
+	sessionID INT AUTO_INCREMENT,
+    creationDate DATETIME NOT NULL,
+    summary VARCHAR(512) NOT NULL,
+    project INT NOT NULL,
+    FOREIGN KEY (project) REFERENCES Project(projectID),
+    PRIMARY KEY (sessionID)
+);
+
+CREATE TABLE Member (
+	memberID INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    session INT NOT NULL,
+    FOREIGN KEY (session) REFERENCES Session(sessionID),
+    PRIMARY KEY (memberID, session, email)
+);
